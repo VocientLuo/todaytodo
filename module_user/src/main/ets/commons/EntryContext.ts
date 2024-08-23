868b68de0e629822ba190fd70592c45e328cac11
vicent <739284055@qq.com>
2024-08-23 23:52:38 +0800

    修复卡片不刷新问题

--- module_user/src/main/ets/commons/EntryContext.ts
+++ module_user/src/main/ets/commons/EntryContext.ts
@@ -1,14 +1,11 @@
 export default class EntryContext {
-  private static context: object
+  private static context: object | undefined = undefined
 
   static setContext(context: object) {
     this.context = context
   }
 
   static getContext(): object {
-    if (!EntryContext.context) {
-      throw Error("context is not init")
-    }
     return EntryContext.context
   }
 }
